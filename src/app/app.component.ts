06587e52f7139456f8823b53da14cd49703ec4f4
dhayananthdevaraj <dhayan.d3@gmail.com>
2022-11-13 08:44:15 +0530

    health result

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { Component } from '@angular/core';
+export interface pat{
+id : number,
+name : string,
+result : string,
+}@Component({
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'health';
+p : pat [ ] = [
+{
+id:1,
+name:"A",
+result:"P"
+},
+{
+id:2,
+name:"B",
+result:"A"
+},
+]
+filpat : pat [ ] = this.p
+sr(event : any){
+if(event.target.value.length > 0)
+this.filpat = this.p.filter(data => data.result
+=== event.target.value)
+else this.filpat = this.p
+}
 }
